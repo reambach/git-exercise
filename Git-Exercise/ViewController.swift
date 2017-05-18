fba45330b4af87685e20f222305684ca8d1a3166
Rachel Ambacher <rachelambacher@gmail.com>
2017-05-18 09:45:23 -0400

    First commit

---
+++ Git-Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git-Exercise
+//
+//  Created by Rach Ambacher on 2017-05-18.
+//  Copyright © 2017 Rach Ambacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
